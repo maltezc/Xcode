97f835bb738f124a4bda8a37ccde4a8846e3b14f
cflux1990 <cflux.maltez@live.com>
2018-11-11 14:22:26 -0800

    24.322 - How to Place Our Dice in 3D Using Touch

--- 24.315-How to Configure and Setup Your Project for AR/ARDicee/ARDicee/ViewController.swift
+++ 24.315-How to Configure and Setup Your Project for AR/ARDicee/ARDicee/ViewController.swift
@@ -44,16 +44,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
 
 
-//         Create a new scene
-//        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
-//
-//        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
-//
-//            diceNode?.position = SCNVector3(x: <#T##Float##Swift.Float#>, y: <#T##Float##Swift.Float#>, z: -0.1)
-//
-//            sceneView.scene.rootNode.addChildNode(diceNode)
-//
-//        }
+
 
 
         
@@ -85,10 +76,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
             let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
 
-            if !results.isEmpty {
-                print("touched the plane")
-            } else {
-                print("touched somewhere else")
+            if let hitResult = results.first {
+                //         Create a new scene
+                let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+
+                if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+
+                    diceNode.position = SCNVector3(
+                            x: hitResult.worldTransform.columns.3.x,
+                            y: hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
+                            z: hitResult.worldTransform.columns.3.z
+                    )
+
+                    sceneView.scene.rootNode.addChildNode(diceNode)
+
+                }
+
             }
         }
     }
